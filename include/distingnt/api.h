1551dccfde02df40024e506413bf4590dc0e21e8
Andrew Ostler <os@expert-sleepers.co.uk>
2025-02-28 11:03:55 +0000

    added API documentation

--- include/distingnt/api.h
+++ include/distingnt/api.h
@@ -27,6 +27,9 @@ SOFTWARE.
 
 #include <stdint.h>
 
+/*
+ * API version. Plug-ins would typically be compiled against kNT_apiVersionCurrent.
+ */
 enum _NT_version
 {
 	kNT_apiVersion1 			= 1,
@@ -36,27 +39,39 @@ enum _NT_version
 	kNT_apiVersionCurrent 		= kNT_apiVersion3
 };
 
+/*
+ * selector argument to pluginEntry()
+ */
 enum _NT_selector
 {
-	kNT_selector_version,
-	kNT_selector_numFactories,
-	kNT_selector_factoryInfo,
+	kNT_selector_version,			// Return the API version this plug-in was compiled against. Value from _NT_version.
+	kNT_selector_numFactories,		// Return the number of factories defined by this plug-in.
+	kNT_selector_factoryInfo,		// Return a pointer to an _NT_factory.
 };
 
+/*
+ * Text size enum used in drawing.
+ */
 enum _NT_textSize
 {
-	kNT_textTiny,
-	kNT_textNormal,
-	kNT_textLarge,
+	kNT_textTiny,					// Tiny 3x5 pixel font.
+	kNT_textNormal,					// Normal 8 point font, bitmapped.
+	kNT_textLarge,					// Large 21 point font, antialiased.
 };
 
+/*
+ * Text alignment enum used in drawing.
+ */
 enum _NT_textAlignment
 {
-	kNT_textLeft,
-	kNT_textCentre,
-	kNT_textRight,
+	kNT_textLeft,					// Left justified.
+	kNT_textCentre,					// Centre justified.
+	kNT_textRight,					// Right justified.
 };
 
+/*
+ * Shape choice enum used in drawing.
+ */
 enum _NT_shape
 {
 	kNT_point,
@@ -65,12 +80,18 @@ enum _NT_shape
 	kNT_rectangle,		// filled
 };
 
+/*
+ * Macro to construct the four character guids used to identify plug-ins.
+ */
 #define NT_MULTICHAR( a, b, c, d )	( ( (uint32_t)a << 0 ) | ( (uint32_t)b << 8 ) | ( (uint32_t)c << 16 ) | ( (uint32_t)d << 24 ) )
 
 #if !defined(ARRAY_SIZE)
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #endif
 
+/*
+ * Globals structure. Plug-ins may always rely on data in the global NT_globals.
+ */
 struct _NT_globals
 {
 	uint32_t	sampleRate;				// sample rate in Hz
@@ -79,39 +100,54 @@ struct _NT_globals
 	uint32_t	workBufferSizeBytes;	// size of workBuffer in bytes
 };
 
+/*
+ * Structure used by calculateStaticRequirements() and initialise().
+ */
 struct _NT_staticRequirements
 {
-	uint32_t	dram;
+	uint32_t	dram;					// Amount of DRAM required.
 };
 
+/*
+ * Structure used by initialise().
+ */
 struct _NT_staticMemoryPtrs
 {
-	uint8_t*	dram;
+	uint8_t*	dram;					// Pointer to DRAM allocated to the plug-in.
 };
 
+/*
+ * Structure used by calculateRequirements() and construct().
+ */
 struct _NT_algorithmRequirements
 {
-	uint32_t	numParameters;
-	uint32_t	sram;
-	uint32_t	dram;
-	uint32_t	dtc;
-	uint32_t	itc;
+	uint32_t	numParameters;			// The number of parameters this plug-in has.
+	uint32_t	sram;					// Amount of SRAM required.
+	uint32_t	dram;					// Amount of DRAM required. Use for large allocations.
+	uint32_t	dtc;					// Amount of DTC required. Use for performance-critical data allocations.
+	uint32_t	itc;					// Amount of ITC required. Use for code only.
 };
 
+/*
+ * Structure used by construct().
+ */
 struct _NT_algorithmMemoryPtrs
 {
-	uint8_t*	sram;
-	uint8_t*	dram;
-	uint8_t*	dtc;
-	uint8_t*	itc;
+	uint8_t*	sram;					// Pointer to SRAM allocated to the plug-in.
+	uint8_t*	dram;					// Pointer to DRAM allocated to the plug-in.
+	uint8_t*	dtc;					// Pointer to DTC allocated to the plug-in.
+	uint8_t*	itc;					// Pointer to ITC allocated to the plug-in.
 };
 
+/*
+ * Values for the unit field of _NT_parameter.
+ */
 enum
 {
 	kNT_unitNone,
-	kNT_unitEnum,
+	kNT_unitEnum,						// enumStrings must also be provided
 	kNT_unitDb,
-	kNT_unitDb_minInf,
+	kNT_unitDb_minInf,					// minimum value will be shown as "-inf" dB
 	kNT_unitPercent,
 	kNT_unitHz,
 	kNT_unitSemitones,
@@ -124,6 +160,7 @@ enum
 	kNT_unitVolts,
 	kNT_unitBPM,
 
+	// These values should only be used via the NT_PARAMETER_AUDIO_INPUT() etc. macros.
 	kNT_unitAudioInput = 100,
 	kNT_unitCvInput,
 	kNT_unitAudioOutput,
@@ -131,6 +168,9 @@ enum
 	kNT_unitOutputMode,
 };
 
+/*
+ * Values for the scaling field of _NT_parameter.
+ */
 enum
 {
 	kNT_scalingNone,
@@ -139,35 +179,52 @@ enum
 	kNT_scaling1000,
 };
 
+/*
+ * Structure to define an algorithm parameter.
+ */
 struct _NT_parameter
 {
-	const char* 			name;
-	int16_t	 				min;
-	int16_t					max;
-	int16_t					def;
-	uint8_t     			unit;
-	uint8_t					scaling;
-	char const * const *	enumStrings;
+	const char* 			name;			// Name
+	int16_t	 				min;			// Minimum value
+	int16_t					max;			// Maximum value
+	int16_t					def;			// Default value
+	uint8_t     			unit;			// Unit
+	uint8_t					scaling;		// Scaling
+	char const * const *	enumStrings;	// Array of strings, if unit is kNT_unitEnum
 };
 
+/*
+ * Structure to define a page of parameters.
+ */
 struct _NT_parameterPage
 {
-	const char* 	name;
-	uint8_t			numParams;
-	const uint8_t*	params;
+	const char* 	name;					// Name of the page
+	uint8_t			numParams;				// Number of parameters on this page
+	const uint8_t*	params;					// Pointer to array of parameter indices
 };
 
+/*
+ * Structure to define all the parameter pages of an algorithm.
+ */
 struct _NT_parameterPages
 {
-	uint32_t					numPages;
-	const _NT_parameterPage*	pages;
+	uint32_t					numPages;	// Number of pages
+	const _NT_parameterPage*	pages;		// Pointer to array of pages
 };
 
+// These two macros are used to build the following macros
 #define NT_PARAMETER_IO( n, m, d, u )	\
 		{ .name = n, .min = m, .max = 28, .def = d, .unit = u, .scaling = 0, .enumStrings = NULL },
 #define NT_PARAMETER_OUTPUT_MODE( n )	\
 		{ .name = n, .min = 0, .max = 1, .def = 0, .unit = kNT_unitOutputMode, .scaling = 0, .enumStrings = NULL },
 
+/*
+ * Macros to define algorithm inputs and outputs.
+ *
+ * n	Parameter name
+ * m	Parameter minimum (typically 0 if no input is allowed, otherwise 1)
+ * d	Default value.
+ */
 #define NT_PARAMETER_AUDIO_INPUT( n, m, d )		NT_PARAMETER_IO( n, m, d, kNT_unitAudioInput )
 #define NT_PARAMETER_CV_INPUT( n, m, d )		NT_PARAMETER_IO( n, m, d, kNT_unitCvInput )
 #define NT_PARAMETER_AUDIO_OUTPUT( n, m, d )	NT_PARAMETER_IO( n, m, d, kNT_unitAudioOutput )
@@ -179,36 +236,110 @@ struct _NT_parameterPages
 	NT_PARAMETER_IO( n, m, d, kNT_unitCvOutput )		\
 	NT_PARAMETER_OUTPUT_MODE( n " mode" )
 
+/*
+ * Structure that defines an algorithm instance.
+ * Plug-ins will typically sub-class this.
+ */
 struct _NT_algorithm
 {
-	const _NT_parameter*		parameters;
-	const _NT_parameterPages*	parameterPages;
-	const int16_t*				vIncludingCommon;
-	const int16_t*				v;
+	const _NT_parameter*		parameters;			// Pointer to the algorithm's parameters. Populate in construct().
+	const _NT_parameterPages*	parameterPages;		// Pointer to the algorithm's parameter pages. Populate in construct().
+	const int16_t*				vIncludingCommon;	// Pointer to the algorithm's parameter values. Managed by the system. Includes common parameters e.g. bypass.
+	const int16_t*				v;					// Pointer to the algorithm's parameter values. Managed by the system. Starts at the algorithm's own first parameter.
 };
 
+/*
+ * Structure that defines an algorithm factory.
+ *
+ * Returned from pluginEntry().
+ *
+ * All plug-in guids should include at least one capital letter. All-lower-case guids are reserved for built-in algorithms.
+ */
 struct _NT_factory
 {
-	uint32_t		guid;
-	const char*		name;
-	const char*		description;
+	uint32_t		guid;				// Algorithm guid. Use NT_MULTICHAR(). Four ASCII characters - stored in the JSON as a string.
+	const char*		name;				// Algorithm name
+	const char*		description;		// Algorithm description
 
+	/*
+	 * Called during plug-in scan, and loading.
+	 * This call gives the plug-in an opportunity to reserve memory that will be shared by all plug-in instances,
+	 * for example lookup tables.
+	 */
 	void			(*calculateStaticRequirements)( _NT_staticRequirements& req );
+
+	/*
+	 * Called during plug-in scan, and loading.
+	 * req is the structure that the plug-in populated in calculateStaticRequirements().
+	 * ptrs contains pointers to memory allocations that satisfy the requirements.
+	 * Typically this call would prepare any data shared by all instances of the plug-in.
+	 */
 	void			(*initialise)( _NT_staticMemoryPtrs& ptrs, const _NT_staticRequirements& req );
+
+	/*
+	 * Called by the system to determine how much memory a new plug-in instance will require.
+	 * This will be during preset loading, or when browsing the 'add algorithm' or 'respecify' menus.
+	 * It should be a lightweight function as it may be called frequently.
+	 */
 	void			(*calculateRequirements)( _NT_algorithmRequirements& req );
+
+	/*
+	 * Called to construct a new plug-in instance. Not optional - this method must be implemented.
+	 * req is the structure that the plug-in populated in calculateRequirements().
+	 * ptrs contains pointers to memory allocations that satisfy the requirements.
+	 * The function should use one of the allocations to prepare an _NT_algorithm (or subclass thereof),
+	 * and return a pointer to this object.
+	 */
 	_NT_algorithm*	(*construct)( const _NT_algorithmMemoryPtrs& ptrs, const _NT_algorithmRequirements& req );
+
+	/*
+	 * Called by the host when a parameter value changes.
+	 * p is the parameter index.
+	 * Typically the plug-in might use the value in self->v[] to compute and cache some derived value.
+	 */
 	void			(*parameterChanged)( _NT_algorithm* self, int p );
+
+	/*
+	 * Primary audio rendering call.
+	 * busFrames gives access to all 28 busses.
+	 * numFramesBy4 is the number of frames to render, divided by 4.
+	 * The busses are arranged one after the other.
+	 * i.e. numFrames of bus 0, then numFrames of bus 1, etc.
+	 */
 	void 			(*step)( _NT_algorithm* self, float* busFrames, int numFramesBy4 );
+
+	/*
+	 * Called by the host to allow the plug-in to render a custom display.
+	 * Return true to suppress the standard parameter line that is drawn at the top of the screen.
+	 */
     bool			(*draw)( _NT_algorithm* self );
+
+    /*
+     * Called by the host to allow the plug-in to respond to realtime MIDI messages e.g. clock.
+     */
     void			(*midiRealtime)( _NT_algorithm* self, uint8_t byte );
+
+    /*
+     * Called by the host to allow the plug-in to respond to MIDI messages.
+     * This will only be called for messages 0x80 to 0xe0, the typical
+     * two or three byte messages that include a MIDI channel.
+     */
     void			(*midiMessage)( _NT_algorithm* self, uint8_t byte0, uint8_t byte1, uint8_t byte2 );
 };
 
 extern "C" {
 
+/*
+ * Primary interface between system and plug-in, used to obtain the factories.
+ */
 typedef uintptr_t (_pluginEntry)( _NT_selector selector, uint32_t data );
 uintptr_t 	pluginEntry( _NT_selector selector, uint32_t data );
 
+/*
+ * Provided mainly as a convenience for Faust programs.
+ *
+ * Given float parameter requirements, populate the _NT_parameter fields scaling, min, max, and def.
+ */
 void		NT_setParameterRange( _NT_parameter* ptr, float init, float min, float max, float step );
 
 // global structure - always available
